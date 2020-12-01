62f85187d27ce6de25f914cbeeb9eb5012faebcc
Willian Garcez Mira <willian.mira@gmail.com.br>
2020-11-30 21:23:27 -0300

    ajuste git ignore

--- Model.Crud.NetCore.Domain/Model/BaseResponseSwagger.cs
+++
@@ -1,9 +0,0 @@
-namespace Model.Crud.NetCore.Domain.Model
-{
-    public class BaseResponseSwagger<T>: BaseResponse
-    {
-        public BaseResponseSwagger(T t) => Conteudo = t;
-
-        public T Conteudo { get; set; }
-    }
-}
